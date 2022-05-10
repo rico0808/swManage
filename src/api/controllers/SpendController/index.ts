65bd6780a90fd8c2efd01ca817729eaef65289f4
Rico <rico0808@126.com>
2022-05-10 23:18:07 +0800

    feat: 支出模块增加

---
+++ src/api/controllers/SpendController/index.ts
@@ -0,0 +1,32 @@
+import { SpendLog } from "@/api/entity/SpendLog";
+import { CheckCookie, CheckPermission } from "@/api/middleware/middleware";
+import { mSpend } from "@/api/utils/model";
+import { onResult } from "@/api/utils/tools";
+import { zPage } from "@/api/utils/zod";
+import { Context, OnPage } from "@/types";
+import { Api, ApiConfig, Post, useContext, Validate } from "@midwayjs/hooks";
+import _ from "lodash";
+import { z } from "zod";
+
+export const config: ApiConfig = {
+  middleware: [CheckCookie, CheckPermission],
+};
+
+const Path = (code: string) => `/api/spend/${code}`;
+
+export const SpendGetSpend = Api(
+  Post(Path("list")),
+  Validate(zPage),
+  async ({ pageSize, current }: z.infer<typeof zPage>): OnPage<SpendLog[]> => {
+    const { session } = useContext<Context>();
+    const { userId, isAdmin } = session;
+    const [list, count] = await mSpend().findAndCount({
+      where: { userId: isAdmin ? null : userId },
+      order: { id: "DESC" },
+      take: pageSize,
+      skip: (current - 1) * pageSize,
+    });
+
+    return onResult({ list, count });
+  }
+);
--- src/views/dealer/myIncome.tsx
+++ src/views/dealer/myIncome.tsx
@@ -1,8 +1,47 @@
+import { Table, Tag } from "@arco-design/web-vue";
+import type { TableColumnData } from "@arco-design/web-vue";
 import { defineComponent } from "vue";
+import usePagination from "@/hooks/usePagination";
+import { SpendGetSpend } from "@/api/controllers/SpendController";
+import dayjs from "dayjs";
 
 export default defineComponent(() => {
+  const { dataList, pagination, onChange, loading } = usePagination(SpendGetSpend);
+
+  const columns: Array<TableColumnData> = [
+    { title: "ID", dataIndex: "id", ellipsis: true, width: 70 },
+    { title: "类型", slotName: "type", width: 100, align: "center" },
+    { title: "金额", slotName: "amount", align: "center" },
+    { title: "详细信息", dataIndex: "remark", ellipsis: true },
+    { title: "创建时间", slotName: "createAt", ellipsis: true, align: "center" },
+  ];
+
   const render = () => {
-    return <div>dealer income</div>;
+    return (
+      <div class="table__card">
+        <Table
+          columns={columns}
+          data={dataList.value}
+          loading={loading.value}
+          pagination={pagination}
+          onPageSizeChange={onChange.pageSize}
+          onPageChange={onChange.current}
+          v-slots={{
+            type: ({ record }) => {
+              return record.type ? (
+                <Tag color="green">收入</Tag>
+              ) : (
+                <Tag color="orange">支出</Tag>
+              );
+            },
+            amount: ({ record }) => `${record.amount} ¥`,
+            createAt: ({ record }) => {
+              return dayjs(record.createAt).format("YYYY-MM-DD HH:mm:ss");
+            },
+          }}
+        />
+      </div>
+    );
   };
 
   return render;
